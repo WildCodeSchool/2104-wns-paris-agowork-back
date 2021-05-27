96f75567117fd15d9e065d503588a14317e7dfbb
Karim Makhloufi <karim.makhloufi.paris@gmail.com>
2021-05-27 10:42:38 +0200

    removed id for usermodel

--- src/model/UserModel.ts
+++ src/model/UserModel.ts
@@ -1,14 +1,10 @@
 import * as mongoose from "mongoose";
 const Schema = mongoose.Schema;
 
-const UserSchema = new Schema( 
-    {
-        id: String,
-        firstname: String, 
-        lastname: String
-    }
-); 
+const UserSchema = new Schema({
+  firstname: String,
+  lastname: String,
+});
 
-
-const model:mongoose.Model<any> =  mongoose.model("user", UserSchema);
+const model: mongoose.Model<any> = mongoose.model("user", UserSchema);
 export default model;
