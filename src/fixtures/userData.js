3ed89880831201e3dbbcebbe8aa5e039d0d020fd
LE DINH Alban <79143540+LEDINHAlban@users.noreply.github.com>
2021-09-07 14:12:06 +0200

    try: 2d

--- src/fixtures/userData.js
+++ src/fixtures/userData.js
@@ -1,79 +1,82 @@
 // node -e 'require(\"./src/fixtures/userData.js\").createUser()'
-const bcrypt = require("bcryptjs");
-const { connect } = require("mongoose");
+// const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
+const schema = mongoose.Schema;
 require("dotenv").config();
 
-const getToken = (payload) => {
-  const token = jwt.sign(payload, secret, {
-    expiresIn: 604800, // 1 Week
-  })
-  return token
-}
+// const getToken = (payload) => {
+//   const token = jwt.sign(payload, secret, {
+//     expiresIn: 604800, // 1 Week
+//   })
+//   return token
+// }
 
 module.exports.createUser = async function () {
+    console.log("toto");
   try {
-    const dbUrl = `mongodb://mongodb:27017/agowork`;
+    const dbUrl = `mongodb://127.0.0.1:27017/agowork`;
     const options = {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       autoIndex: true
     };
-    await connect(dbUrl, options);
-    const user = connect.Schema({
+    await mongoose.connect(dbUrl, options);
+    console.log("await connect");
+    const user = new schema({
       firstname: {
         type: String,
         unique: true,
       },
-      lastname: {
-        type: String,
-        unique: true,
-      },
-      email: {
-        type: String,
-        unique: true,
-      },
-      town: String,
-      picture: String,
-      role: String,
-      password: String,
-      token: String,
+    //   lastname: {
+    //     type: String,
+    //     unique: true,
+    //   },
+    //   email: {
+    //     type: String,
+    //     unique: true,
+    //   },
+    //   town: String,
+    //   picture: String,
+    //   role: String,
+    //   password: String,
+    //   token: String,
     });
     connect.model("User", user);
-    const password = "password";
-    const hashedPassword = await bcrypt.hashSync(password, 12);
-    const picture = "https://images.unsplash.com/photo-1627434880836-e94b1bdc2098?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60";
-    const cities = ["Paris", "Londres", "Madrid", "Moscou", "New York", "Long Beach", "Los Angeles", "Marseille", "Nice", "Grenoble", "Brest"];
+    // const password = "password";
+    // const hashedPassword = await bcrypt.hashSync(password, 12);
+    // const picture = "https://images.unsplash.com/photo-1627434880836-e94b1bdc2098?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyMnx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=60";
+    // const cities = ["Paris", "Londres", "Madrid", "Moscou", "New York", "Long Beach", "Los Angeles", "Marseille", "Nice", "Grenoble", "Brest"];
     for (let i = 0; i < 20; i++) {
       let firstname;
-      let lastname;
-      let email;
-      let role;
+    //   let lastname;
+    //   let email;
+    //   let role;
       firstname = "firstname" + [i];
-      lastname = "lastname" + [i];
-      email = "email" + [i];
-      const payload = { userEmail: email, userRole: role };
-      const token = getToken(payload);
-      const random = Math.floor(Math.random() * cities.length);
-      if (i == 0) {
-        role = 'SUPERADMIN';
-      } else if (i > 0 && i < 5) {
-        role = 'ADMIN';
-      } else if (i >= 5 && i < 10) {
-        role = 'ADMIN';
-      } else if (i >= 10 && i < 15) {
-        role = 'TEACHER';
-      } else if (i >= 15 && i < 20) {
-        role = 'STUDENT';
-      }
+    //   lastname = "lastname" + [i];
+    //   email = "email" + [i];
+    //   const payload = { userEmail: email, userRole: role };
+    //   const token = getToken(payload);
+    //   const random = Math.floor(Math.random() * cities.length);
+    //   if (i == 0) {
+    //     role = 'SUPERADMIN';
+    //   } else if (i > 0 && i < 5) {
+    //     role = 'ADMIN';
+    //   } else if (i >= 5 && i < 10) {
+    //     role = 'ADMIN';
+    //   } else if (i >= 10 && i < 15) {
+    //     role = 'TEACHER';
+    //   } else if (i >= 15 && i < 20) {
+    //     role = 'STUDENT';
+    //   }
       const body = {
         firstname: firstname,
-        lastname: lastname,
-        town: cities[random],
-        email: email,
-        picture: picture,
-        role: role,
-        password: hashedPassword,
-        token: token,
+        // lastname: lastname,
+        // town: cities[random],
+        // email: email,
+        // picture: picture,
+        // role: role,
+        // password: hashedPassword,
+        // token: token,
       };
       const user = new User(body);
     }
