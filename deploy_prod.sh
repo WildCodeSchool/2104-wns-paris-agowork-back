8f5de81a4482ac8ee7d1bbf86f42209b17efe37c
Oceane-Kolau <Oceane-Kolau@users.noreply.github.com>
2021-10-06 15:39:09 +0200

    update

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 echo "PORT=$PORT"
 git fetch origin && git reset --hard origin/master && git clean -f -d
-GATEWAY_PORT=$PORT docker-compose -f docker-compose-prod.yml up --build -d
+(cd ../2104-wns-paris-agowork-front ; GATEWAY_PORT=$PORT docker-compose -f docker-compose-prod.yml up --build -d)
