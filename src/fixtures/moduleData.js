4b113941b0395d3d3c79632b05b0122a9b2acc2b
Oceane-Kolau <Oceane-Kolau@users.noreply.github.com>
2021-11-04 12:42:04 +0100

    add env variables for db connection and fix node version because of webpack error with node17

--- src/fixtures/moduleData.js
+++ src/fixtures/moduleData.js
@@ -1,12 +1,12 @@
 // command to initialize users in database
-// node -e 'require(\"./src/Fixtures/userData.js\").createUser()'
-const bcrypt = require("bcryptjs");
+// node -e 'require(\"./src/Fixtures/moduleData.js\").createModule()'
 const mongoose = require("mongoose");
 require("dotenv").config();
 
 module.exports.createModule = async function () {
   try {
-    const dbUrl = `mongodb://mongodb:27017/agowork`;
+    const dbLink = process.env.DB_LINK;
+    const dbUrl = 'mongodb://' + dbLink + ':27017/agowork';
     const options = {
       useCreateIndex: true,
       useNewUrlParser: true,
@@ -20,20 +20,15 @@ module.exports.createModule = async function () {
         unique: true,
       },
       content: String,
-
-    //   course: {
-    //     type: Array,
-    //     unique: true
-    //   }
     }))
 
     const comment = [
-                        "JavaScript, often abbreviated as JS, is a high-level, interpreted programming.",
-                        "React est une bibliothèque JavaScript permettant de construire des interfaces.",
-                        "TypeScript is an open-source programming language. It is a strict syntactical.",
-                        "React Native - Build native iOS, Android and Windows apps with JavaScript.",
-                        "Découvre comment utiliser les class components en React",
-                    ]
+      "JavaScript, often abbreviated as JS, is a high-level, interpreted programming.",
+      "React est une bibliothèque JavaScript permettant de construire des interfaces.",
+      "TypeScript is an open-source programming language. It is a strict syntactical.",
+      "React Native - Build native iOS, Android and Windows apps with JavaScript.",
+      "Découvre comment utiliser les class components en React",
+    ]
 
     if (ModuleModel.count() !== 0) {
       await ModuleModel.deleteMany();
